5cf9ee726ed1176d69e52b80e50d5b36eceb2249
Sean Sutton <sutton_mjbc@icloud.com>
2020-05-20 15:03:56 -0400

    header assets and initial hero component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Header.css";
+import LittleShredIcon from "../../assets/Little-Shred-Icon.png";
+import shredLogo from "../../assets/shred-logo.png";
 
 const Header = () => (
   <div className="header">
-    <p>Header</p>
+    <img className="logo-img" src={LittleShredIcon} alt="Icon" />
+    <br />
+    <br />
+    <img className="shred-logo" src={shredLogo} alt="Icon" />
   </div>
 );
 
