264f2bb6898ab438b32ad2c993355b86c9a98c81
Sean Sutton <sutton_mjbc@icloud.com>
2020-05-22 13:44:28 -0400

    initial schedule component

---
+++ src/components/Schedule/Schedule.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Scbedule extends React.Component {
+  componentWillMount() {
+    //
+    //
+  }
+
+  componentWillUnmount() {}
+
+  render() {
+    return <div></div>;
+  }
+}
